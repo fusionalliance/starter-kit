ca2123669cd9235a0c40fe2b7c552029a23166c2
Ragsdale, David <dragsdale@fusionalliance.com>
2019-11-18 07:59:50 -0500

    Add webpack build

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,8 @@
 module.exports = {
-  "extends": "airbnb-base"
+  extends: 'airbnb-base',
+  rules: {
+    'import/no-extraneous-dependencies': ['error', {
+      devDependencies: true,
+    }],
+  },
 };
