95af60d2b81d306c4d82ca1f15b02d540ffb7cb4
Ragsdale, David <dragsdale@fusionalliance.com>
2020-04-13 09:35:04 -0400

    Have postcss config use browserslistrc

--- postcss.config.js
+++ postcss.config.js
@@ -20,11 +20,10 @@ module.exports = {
      * https://github.com/postcss/autoprefixer
      */
     autoprefixer({
-      /* It should add vendor prefixes for the last 2 versions of all browsers, meaning old prefixes such as
-       * -webkit-border-radius: 5px; that the latest browsers support as border-radius won't be added.
-       * https://github.com/ai/browserslist#queries
+      /* It should add vendor prefixes for the browsers indicated in .browserslistrc, meaning old
+       * prefixes such as -webkit-border-radius: 5px; that the latest browsers support as
+       * border-radius won't be added. https://github.com/ai/browserslist#queries
        */
-      browsers: 'last 2 versions',
     }),
   ],
 };
