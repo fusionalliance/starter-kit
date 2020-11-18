52deece2a758b0f8c0e572d7913cf327d7e7ec50
Scott Colegrove <colegrove_scott@network.lilly.com>
2020-11-18 10:02:54 -0500

    Clean up package.json at the end

--- src/frameworks/vue/index.js
+++ src/frameworks/vue/index.js
@@ -74,5 +74,23 @@ module.exports = async function vue() {
   this.log('Adding scripts to package.json');
   await this.extendJson(this.destinationPath('package.json'), packageJsonScripts);
 
+  this.log('Cleaning up package.json');
+  const destinationPkgJson = require(this.destinationPath('package.json')); // eslint-disable-line global-require, import/no-dynamic-require
+  delete destinationPkgJson.eslintConfig;
+
+  const devDeps = {};
+  const deps = {};
+  Object.keys(destinationPkgJson.devDependencies).sort().forEach((key) => {
+    devDeps[key] = destinationPkgJson.devDependencies[key];
+  });
+
+  Object.keys(destinationPkgJson.dependencies).sort().forEach((key) => {
+    deps[key] = destinationPkgJson.dependencies[key];
+  });
+  destinationPkgJson.devDependencies = devDeps;
+  destinationPkgJson.dependencies = deps;
+
+  await this.writeJson(this.destinationPath('package.json'), destinationPkgJson);
+
   await fse.copy(this.destinationPath('.env.sample'), this.destinationPath('.env'));
 };
