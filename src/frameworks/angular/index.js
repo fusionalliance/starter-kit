ef9838b1a49e2f32b2dc44969fb81fa66d7bf902
Scott Colegrove <colegrove_scott@network.lilly.com>
2021-04-06 13:30:00 -0400

    Setup angular project with eslint

--- src/frameworks/angular/index.js
+++ src/frameworks/angular/index.js
@@ -3,13 +3,25 @@ const inquirer = require('inquirer');
 const path = require('path');
 
 /* eslint-disable no-useless-escape */
-// const packageJsonScripts = {
-//   scripts: {
-//     stylelint: 'stylelint \"src/assets/scss/**/*.scss\"',
-//     lint: 'vue-cli-service lint && npm run stylelint',
-//   },
-// };
+const packageJsonScripts = {
+  scripts: {
+    stylelint: 'stylelint \"**/*.scss\"',
+    lint: 'ng lint && npm run stylelint',
+  },
+};
 /* eslint-enable */
+
+const dependencies = {
+  dependencies: {
+  },
+  devDependencies: {
+    stylelint: '^11.1.1',
+    'stylelint-config-recommended': '^3.0.0',
+    'stylelint-config-recommended-scss': '^4.0.0',
+    'stylelint-config-standard': '^19.0.0',
+    'stylelint-scss': '^3.12.1',
+  },
+};
 
 module.exports = async function angular() {
   const { projectName } = await inquirer.prompt({
@@ -18,8 +30,16 @@ module.exports = async function angular() {
     message: 'Enter the folder name for your project',
   });
 
-  // saying no to strict mode https://angular.io/guide/strict-mode
-  await this.asyncCommand('npx', ['@angular/cli@11.x', 'new', projectName, '--routing', '--no-strict', '--style', 'scss']);
+  const pathToModules = path.resolve(__dirname, '../../../node_modules');
+
+  // https://angular.io/cli/new#options
+  await this.asyncCommand('npx', ['@angular/cli@11.x',
+    'new', projectName,
+    '--collection', `${pathToModules}/@angular-eslint/schematics`, // https://github.com/angular-eslint/angular-eslint - tslint is deprecated
+    '--routing',
+    '--no-strict', // https://angular.io/guide/strict-mode
+    '--style', 'scss',
+  ]);
   process.chdir(projectName); // change directory to new folder
 
   this.log('Copying common files');
@@ -33,4 +53,10 @@ module.exports = async function angular() {
   //   path.join(__dirname, './template'),
   //   this.destinationPath(),
   // );
+
+  this.log('Adding dependencies to package.json');
+  await this.extendJson(this.destinationPath('package.json'), dependencies);
+
+  this.log('Adding scripts to package.json');
+  await this.extendJson(this.destinationPath('package.json'), packageJsonScripts);
 };
